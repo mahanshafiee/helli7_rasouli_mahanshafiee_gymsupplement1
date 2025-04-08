fb7dee6dc48a3396eb6327e4405117004e177be7
mahanshafiee <helpmegetac200@gmail.com>
2025-04-08 13:21:39 +0330

    now making website responsive completely . i made header reponsive and added hamburger menu

---
+++ script.js
@@ -0,0 +1,8 @@
+/*hamburger section*/
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
+
+hamburger.onclick = function () {
+  navMenu.classList.toggle('active');
+  this.classList.toggle('active');
+};
