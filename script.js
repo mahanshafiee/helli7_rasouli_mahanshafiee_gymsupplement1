7ad288f97c03fdffaa6f9e86d2f24a3b5870c198
mahanshafiee <helpmegetac200@gmail.com>
2025-04-09 09:54:21 +0330

    daily timer is now responsive

--- script.js
+++ script.js
@@ -10,3 +10,39 @@ hamburger.onclick = function () {
 
 /*daily timer*/
 
+let d = document.getElementById("days");
+let h = document.getElementById("hours");
+let m = document.getElementById("minute");
+let s = document.getElementById("second");
+
+d.innerHTML = 1;
+h.innerHTML = 23;
+m.innerHTML = 59;
+s.innerHTML = 59;
+
+function count() {
+    let sec = s.innerHTML;
+    sec--;
+    s.innerHTML = sec;
+
+    if (sec < 0) {
+        s.innerHTML = 59;
+        m.innerHTML--;
+    }
+    if (m.innerHTML < 0) {
+        m.innerHTML = 59;
+        h.innerHTML--;
+    }
+    if (h.innerHTML < 0) {
+        h.innerHTML = 23;
+        d.innerHTML--;
+    }
+    if (d.innerHTML < 0) {
+        d.innerHTML = 0;
+        h.innerHTML = 0;
+        m.innerHTML = 0;
+        s.innerHTML = 0;
+    }
+}
+
+setInterval(count, 1000);
